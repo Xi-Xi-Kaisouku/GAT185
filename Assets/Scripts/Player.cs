0cec240ab055b8be636e64ead569b25d2061fba8
Xi-Xi <358xixi@gmail.com>
2021-01-16 16:14:32 -0700

    GAT185
    Day 2 Done

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity.z = Input.GetAxis("Vertical");
+
+        transform.position += velocity * speed * Time.deltaTime;
+    }
+}
